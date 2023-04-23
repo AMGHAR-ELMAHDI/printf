712251981ee27761aaa0c97ed24c2da908608f42
ELMAHDI AMGHAR <amgharmahdi1@gmail.com>
2023-04-23 16:35:20 +0000

    printf

--- _printf_string.c
+++ _printf_string.c
@@ -9,6 +9,9 @@
  */
 void _printf_string(char *s, int *len)
 {
+	char hex[2];
+	int i;
+
 	if (s == NULL)
 	{
 		_printf_string("(null)", len);
@@ -17,10 +20,24 @@ void _printf_string(char *s, int *len)
 
 	while (*s)
 	{
-		_printf_char(*s, len);
+		if (*s < 32 || *s >= 127)
+		{
+			_printf_char('\\', len);
+			_printf_char('x', len);
+			hex[0] = (*s >> 4) & 0xF;
+			hex[1] = *s & 0xF;
+			for (i = 0; i < 2; i++)
+			{
+				if (hex[i] < 10)
+					_printf_char('0' + hex[i], len);
+				else
+					_printf_char('A' + (hex[i] - 10), len);
+			}
+		}
+		else
+			_printf_char(*s, len);
 		s++;
 	}
-
 	_printf_char('\0', len);
 }
 
